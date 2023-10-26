afd50011b69c44a1b2a69728c5bb45c19ea4a7be
Lolozix <lorranasilvadamasceno@gmail.com>
2023-10-26 13:57:54 -0400

    commit inicial

--- app.js
+++ app.js
@@ -6,12 +6,26 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index', { message: 'Olá, Mundo!' });
+  res.render('index', { message: 'produtos' });
 });
 
 app.get('/produtos', (req, res) => {
-  res.render('produtos', { message: 'Olá, Mundo!' });
+  res.render('produtos', { message: 'produtoDestaque' });
 });
+
+app.get('/produto1', (req, res) => {
+  res.render('produto1', { message: 'sobre1' });
+});
+
+app.get('/produto2', (req, res) => {
+  res.render('produto2', { message: 'sobre2' });
+});
+
+app.get('/produto3', (req, res) => {
+  res.render('produto3', { message: 'sobr3' });
+});
+
+
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
