af574286623c0c246997f5fa12feaf34ed157048
Lolozix <lorranasilvadamasceno@gmail.com>
2023-11-16 14:18:03 -0400

    commit 16.11

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+const produtos = [
+  {id:1, nome:"Camisa Corinthians I 23/24 s/n° Torcedor Nike Masculina - Branco", preco:"399,99", descricao:"Celebre os 40 anos de um movimento que ajudou a moldar a trajetória política do Brasil"},
+  {id:2, nome:"Camisa Corinthians I 23/24 s/n° Torcedor Nike Masculina - Branco", preco:"399,99", descricao:"Celebre os 40 anos de um movimento que ajudou a moldar a trajetória política do Brasil"},
+  {id:3, nome:"Camisa Corinthians I 23/24 s/n° Torcedor Nike Masculina - Branco", preco:"399,99", descricao:"Celebre os 40 anos de um movimento que ajudou a moldar a trajetória política do Brasil"},
+  {id:4, nome:"Camisa Corinthians I 23/24 s/n° Torcedor Nike Masculina - Branco", preco:"399,99", descricao:"Celebre os 40 anos de um movimento que ajudou a moldar a trajetória política do Brasil"},
+  {id:5, nome:"Camisa Corinthians I 23/24 s/n° Torcedor Nike Masculina - Branco", preco:"399,99", descricao:"Celebre os 40 anos de um movimento que ajudou a moldar a trajetória política do Brasil"},
+  {id:6, nome:"Camisa Corinthians I 23/24 s/n° Torcedor Nike Masculina - Branco", preco:"399,99", descricao:"Celebre os 40 anos de um movimento que ajudou a moldar a trajetória política do Brasil"},
+  {id:7, nome:"Camisa Corinthians I 23/24 s/n° Torcedor Nike Masculina - Branco", preco:"399,99", descricao:"Celebre os 40 anos de um movimento que ajudou a moldar a trajetória política do Brasil"},
+  {id:8, nome:"Camisa Corinthians I 23/24 s/n° Torcedor Nike Masculina - Branco", preco:"399,99", descricao:"Celebre os 40 anos de um movimento que ajudou a moldar a trajetória política do Brasil"},
+  {id:9, nome:"Camisa Corinthians I 23/24 s/n° Torcedor Nike Masculina - Branco", preco:"399,99", descricao:"Celebre os 40 anos de um movimento que ajudou a moldar a trajetória política do Brasil"},
+  {id:10,nome:"Camisa Corinthians I 23/24 s/n° Torcedor Nike Masculina - Branco", preco:"399,99", descricao:"Celebre os 40 anos de um movimento que ajudou a moldar a trajetória política do Brasil"},
+]
+
+function buscarProdutoPorId(id) {
+  const produto = produtos.find(produtos => produto.id == id);
+      return produto || null 
+}   
+
 app.get('/', (req, res) => {
   res.render('index', { message: 'produtos' });
 });
